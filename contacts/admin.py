0781643e754349f688202b6e253988713876b881
pt-ubuntu-os <persian1379pr@gmail.com>
2022-02-02 17:03:10 +0330

    create contact models

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from contacts.models import CustomUser, Group, Contact
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff'
+    )
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(Group)
+admin.site.register(Contact)
--- contacts/models.py
+++ contacts/models.py
@@ -1,9 +1,35 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 # User = get_user_model()
 
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+999999999'.")
+
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.group_name
+
+
+class Contact(models.Model):
+    group = models.ForeignKey(Group, related_name='contacts', on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    phone_number = models.CharField(validators=[phone_regex],
+                                    max_length=11,
+                                    blank=True)
+    address = models.TextField(max_length=256,
+                               default='',
+                               blank=True)
+
+    def __str__(self):
+        return self.user
